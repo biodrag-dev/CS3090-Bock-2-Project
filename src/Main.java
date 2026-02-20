67d4a4732aa08a93ce5785e9e309b2ccb4db28d7
biodegradable-dev <emily.yuluo.wang@gmail.com>
2026-02-19 22:46:56 -0700

    Finished code

--- src/Main.java
+++ src/Main.java
@@ -1,15 +1,54 @@
-//TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
-// click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
+import java.util.Random;
+import java.util.Scanner;
+
 public class Main {
     public static void main(String[] args) {
-        //TIP Press <shortcut actionId="ShowIntentionActions"/> with your caret at the highlighted text
-        // to see how IntelliJ IDEA suggests fixing it.
-        System.out.printf("Hello and welcome!");
+        Scanner sc = new Scanner(System.in);
 
-        for (int i = 1; i <= 5; i++) {
-            //TIP Press <shortcut actionId="Debug"/> to start debugging your code. We have set one <icon src="AllIcons.Debugger.Db_set_breakpoint"/> breakpoint
-            // for you, but you can always add more by pressing <shortcut actionId="ToggleLineBreakpoint"/>.
-            System.out.println("i = " + i);
+        System.out.println("Please enter your choice (0 for encryption, 1 for decryption):");
+        String choice = sc.nextLine();
+        switch (choice) {
+            case "0":
+                System.out.println("Please enter the string to be encrypted");
+                System.out.println(encrypt(sc.next()));
+                break;
+            case "1":
+                System.out.println("Please enter your encrypted string:");
+                System.out.println(decrypt(sc.next()));
+                break;
+            default:
+                System.out.println("Invalid choice. Exiting program now.");
+                break;
         }
+
+    }
+
+    public static String encrypt(String text){
+        StringBuilder encryptedString = new StringBuilder();
+        Random rand = new Random();
+        int seed = rand.nextInt(100);
+        //appends 0 to pad it out properly if needed
+        if(seed == 0){
+            encryptedString.append("000");
+        }else if(seed < 10){
+            encryptedString.append("00");
+        }else if(seed < 100){
+            encryptedString.append("0");
+        }
+        encryptedString.append(seed);
+        for(char character : text.toCharArray()){
+            encryptedString.append((char)(character + seed));
+        }
+        return encryptedString.toString();
+    }
+
+    public static String decrypt(String text){
+        StringBuilder decryptedString = new StringBuilder();
+        int seed = Integer.parseInt(text.substring(0, 3));
+        char[] chars = text.toCharArray();
+        for(int i = 3; i < text.length(); i++){
+            decryptedString.append((char)(chars[i] - seed));
+        }
+        return decryptedString.toString();
     }
 }
